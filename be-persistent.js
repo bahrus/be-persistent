7ce42e434e049b863a8cbe20ea6d52af77f9c04e
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-11-26 16:50:27 -0500

    Merge branch 'baseline' of https://github.com/bahrus/be-persistent into baseline

--- be-persistent.js
+++ be-persistent.js
@@ -1,6 +1,7 @@
 // @ts-check
 import { resolved, rejected, propInfo} from 'be-enhanced/cc.js';
 import { BE } from 'be-enhanced/BE.js';
+import {dispatchEvent as de} from 'trans-render/positractions/dispatchEvent.js';
 
 /** @import {BEConfig, IEnhancement, BEAllProps} from './ts-refs/be-enhanced/types.d.ts' */
 /** @import {Actions, PAP, AllProps, AP, BAP} from './ts-refs/be-persistent/types.d.ts' */;
@@ -11,10 +12,50 @@ import { BE } from 'be-enhanced/BE.js';
  */
 class BePersistent extends BE {
     /**
-     * @type {BEConfig<AP & BEAllProps, Actions & IEnhancement, any>}
+     * @type {BEConfig<BAP, Actions & IEnhancement, any>}
      */
     static config = {
+        propInfo: {
+            ...propInfo,
+            rules: {}
+        },
+        compacts: {
+            when_rules_changes_invoke_hydrate: 0,
+        },
+        positractions: [resolved, rejected],
+        actions: {
+            noAttrs: {
+                ifNoneOf: ['rules']
+            }
+        }
     };
+
+    de = de;
+
+    /**
+     * 
+     * @param {BAP} self 
+     */
+    async noAttrs(self){
+        return /** @type {PAP} */ ({
+            rules: [{
+                localEvent: 'input',
+                localProp: 'value',
+                usl: 'sessionStorage://{autogenID}'
+            }]
+        });
+    }
+
+    /**
+     * 
+     * @param {BAP} self 
+     * @returns 
+     */
+    async hydrate(self){
+        return /** @type {PAP} */ ({
+            resolved: true
+        });
+    }
 }
 
 await BePersistent.bootUp();
