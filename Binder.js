9c9d77d785fb70568545325a9c9d79ae3db83cd3
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-11-27 07:17:59 -0500

    Merge branch 'baseline' of https://github.com/bahrus/be-persistent into baseline

--- Binder.js
+++ Binder.js
@@ -13,6 +13,10 @@ export class Binder{
      */
     #rule;
 
+    /**
+     * @type {WeakRef<BAP>}
+     */
+    #selfRef;
 
     /**
      * @param {BAP} self
@@ -21,9 +25,10 @@ export class Binder{
      */
     constructor(self, rule, ac){
         this.#rule = rule;
+        this.#selfRef = new WeakRef(self);
         const {localEvent} = rule;
         const {enhancedElement} = self;
-        enhancedElement.addEventListener(localEvent || 'input', this);
+        enhancedElement.addEventListener(localEvent || 'input', this, {signal: ac.signal});
         this.handleEvent();
     }
 
@@ -31,7 +36,66 @@ export class Binder{
         return location.origin + location.pathname + '?' + location.search;
     }
 
-    handleEvent(){
-
+    /**
+     * 
+     * @param {Event=} e 
+     */
+    async handleEvent(e){
+        const rule = this.#rule;
+        const self = this.#selfRef.deref();
+        if(self === undefined) return;
+        const {enhancedElement} = self;
+        const {localProp, usl} = rule;
+        const {get} = await import('trans-render/XV/get.js');
+        /**
+         * @type  {import('./ts-refs/trans-render/XV/types').USL}
+         */
+        let staticUSL = usl || 'sessionStorage://{autoGenId}';
+        if(staticUSL.includes('{autoGenId}')){
+            
+        }
+        if(e === undefined){
+            const currentLocalVal = enhancedElement[localProp || 'value'];
+            const currentStoreVal = await get(staticUSL);
+            //initialization
+        }
     }
+}
+
+//move to trans-render:
+const typeRankings = [
+    'undefined',
+    'null',
+    'string',
+    'boolean',
+    'number',
+    'bigint',
+    'symbol',
+    'object',
+    'function'
+];
+function breakTie(lhs, rhs) {
+    if (lhs === rhs)
+        return 'eq';
+    const lhsType = lhs === null ? 'null' : typeof lhs;
+    const rhsType = rhs === null ? 'null' : typeof rhs;
+    const lhsTypeScore = typeRankings.indexOf(lhsType);
+    const rhsTypeScore = typeRankings.indexOf(rhsType);
+    if (lhsTypeScore > rhsTypeScore)
+        return 'lhs';
+    if (rhsTypeScore > lhsTypeScore)
+        return 'rhs';
+    switch (lhsType) {
+        case 'string':
+            if (lhs.length > rhs.length)
+                return 'lhs';
+            if (rhs.length > lhs.length)
+                return 'rhs';
+        default:
+            if (lhs > rhs)
+                return 'lhs';
+            if (rhs > lhs)
+                return 'rhs';
+    }
+    return 'eq';
 }
