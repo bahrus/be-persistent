4f74b08014c31e1bd70b1c3eeefbd8720d0a0e5e
bahrus <anderson.bruce.b@gmail.com>
2026-09-24 07:00:31 -0400

    0.0.32

--- def.js
+++ def.js
@@ -8,15 +8,11 @@ export async function defBePersistent(ref){
     const {BePersistent} = await import('./be-persistent.js');
     const {default: emc} = await import('./emc.json', {with: {type: 'json'}});
     const {enhConfig} = emc;
-    console.log({emc, enhConfig});
     enhConfig.spawn = BePersistent;
     enhConfig.customData = emc.customData;
-    //enhConfig.emc = emc;
     const registry = ref?.customElementRegistry ?? customElements;
     const {enhancementRegistry} = registry;
-    //Object.assign(emc, enhConfig);
     enhancementRegistry.push(enhConfig);
     const {default: emc2} = await import('./emc.json', {with: {type: 'json'}});
-    console.log(emc === emc2);
 
 }
