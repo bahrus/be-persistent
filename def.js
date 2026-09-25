af43ff3d437e04d68c026d64e69b799e7b3a0f23
bahrus <anderson.bruce.b@gmail.com>
2026-09-24 22:38:30 -0400

    0.0.32

--- def.js
+++ def.js
@@ -5,13 +5,23 @@ import  'assign-gingerly/object-extension.js';
  * @param {Element | undefined} ref 
  */
 export async function defBePersistent(ref){
-    const {BePersistent} = await import('./be-persistent.js');
     const {default: emc} = await import('./emc.json', {with: {type: 'json'}});
+    return await push(ref, emc);
+    
+}
+
+// export async function defEmoji(ref){
+//     const {default: emc} = await import('./emc-emoji.json', {with: {type: 'json'}});
+//     await push(ref, emc);
+// }
+
+async function push(ref, emc){
+    const {BePersistent} = await import('./be-persistent.js');
     const {enhConfig} = emc;
     enhConfig.spawn = BePersistent;
     enhConfig.customData = emc.customData;
     const registry = ref?.customElementRegistry ?? customElements;
     const {enhancementRegistry} = registry;
     enhancementRegistry.push(enhConfig);
-
+    return enhConfig;
 }
